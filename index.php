b7400cf2c46044bcfbb144e0f4dfce394e9e13d5
9vies <dragonslayer834@gmail.com>
2017-02-27 15:41:18 -0500

    Inclusion du fichier "connexion.php" dans le code.

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
+<?php
+	include 'connexion.php'; 
+ ?>
 <!doctype html>
 <html lang="fr">
 <head>
 	<meta charset="UTF-8">
-	<title>Acceuil</title>
+	<title>Accueil</title>
 	<link href="css/styles.css" rel="stylesheet" />
 	<script src="js/script.js"></script>
 </head>
