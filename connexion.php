1a07d7b82edb253520abb3cf899c7fa20f1ead87
9vies <dragonslayer834@gmail.com>
2017-02-27 15:31:57 -0500

    Premier essai de transfert de données.

--- connexion.php
+++ connexion.php
@@ -5,14 +5,19 @@
 	<title>Connexion</title>
 </head>
 <body>
-	<form action="traitementconnexion.php" method="post">
-		Nom d'utilisateur: <input type="text" name="username" maxlength="8"><br>
-		Mot de passe: <input type="password" name="password" maxlength="8"><br>
-		<button type="Submit" value="Submit">Connexion</button>
-		<p id="erreur"></p>
-
-
-
-	</form>
+	<div>
+		<form action="traitementconnexion.php" method="post">
+			Nom d'utilisateur: <input type="text" name="username" maxlength="8"><br>
+			Mot de passe: <input type="password" name="password" maxlength="8"><br>
+			<button type="Submit" value="Submit">Connexion</button>
+			<p id="erreur"></p>
+			<?php 
+				if(isset($_POST['erreur']) && !empty($_POST['erreur'])) 
+				{
+				 	echo $_POST['erreur'];
+				} 
+			 ?>	
+		</form>
+	</div>	
 </body>
 </html>
