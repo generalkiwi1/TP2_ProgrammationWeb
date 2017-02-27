582084e00a1e704618e23b333897ddbae254fbfa
generalkiwi1 <xx-mik-xx@hotmail.com>
2017-02-27 15:17:54 -0500

    Version de base qui peut être modifier
    Signed-off-by: generalkiwi1 <xx-mik-xx@hotmail.com>

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Acceuil</title>
+	<link href="css/styles.css" rel="stylesheet" />
+	<script src="js/script.js"></script>
+</head>
+<body>
+</body>
+</html>
---
+++ recherche.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Recherche</title>
+	<link href="css/styles.css" rel="stylesheet" />
+	<script src="js/script.js"></script>
+</head>
+<body>
+</body>
+</html>
