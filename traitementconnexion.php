de87973b43c7bf673d3e9498cce90db3e0612bea
9vies <dragonslayer834@gmail.com>
2017-03-02 12:08:57 -0500

    ^^

--- traitementconnexion.php
+++ traitementconnexion.php
@@ -1,8 +1,25 @@
 <?php 
 	session_start();
 
+	define("USERNAME1", "9vies");
+	define("PASSWORD1", "psswrd12");
+	define("USERNAME2", "Generalkiwi");
+	define("PASSWORD2", "passwrd23");
+
 	$_SESSION['username'] = $_POST['username']; 
 
-	header('Location:connexion.php?erreur=OK');
+	function ValiderUtilisateur()
+	{
+		if ($_POST['username'] == USERNAME1 && $_POST['password'] == PASSWORD1) 
+		{
+			header('Location:connexion.php?erreur=9vies est connecté');
+		}
+		else if ($_POST['username'] == USERNAME2 && $_POST['password'] == PASSWORD2) 
+		{
+			header('Location:connexion.php?erreur=Generalkiwi est connecté');
+		}
+	}
+
+	
 
  ?>
