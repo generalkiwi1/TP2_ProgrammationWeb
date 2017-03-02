63698fc417654879fd366658fa830364ec752c54
9vies <dragonslayer834@gmail.com>
2017-03-02 14:00:48 -0500

    sdasdsada

--- banniere.php
+++ banniere.php
@@ -2,9 +2,7 @@
 	<img id="carousel" src="img/ville/londre.jpg" alt="photo de la ville" />
 </div>
 <div id="contenu">
-	<?php
-		include 'connexion.php'; 
-	 ?>	
+	
 </div> 
 <div class="cleardiv">
 		
