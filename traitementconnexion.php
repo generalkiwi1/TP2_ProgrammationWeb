d600d53dbab075a218fa59b33214fade96e6c988
9vies <dragonslayer834@gmail.com>
2017-03-05 17:52:17 -0500

    Ajout de la spécification de l'erreur à côté du champ concerné durant la connexion.

--- traitementconnexion.php
+++ traitementconnexion.php
@@ -11,27 +11,18 @@
 
 	if ($_SESSION['username'] == USERNAME1 && $_SESSION['password'] == PASSWORD1) 
 	{
-		header('Location:connexion.php?erreur=9vies est connecté');
+		header('Location:connexion.php?connection=9vies est connecté');
 	}
 	else if ($_SESSION['username'] == USERNAME2 && $_SESSION['password'] == PASSWORD2) 
 	{
-		header('Location:connexion.php?erreur=Generalkiwi est connecté');
+		header('Location:connexion.php?connection=Generalkiwi est connecté');
+	}
+	else if ($_SESSION['username'] != USERNAME1 && $_SESSION['username'] != USERNAME2) 
+	{
+		header('Location:connexion.php?erreur1=Nom d\'utilisateur incorrect');
 	}
 	else
 	{
-		header('Location:connexion.php?erreur=Nom d\'utilisateur ou mot de passe incorrect');
+		header('Location:connexion.php?erreur2=Mot de passe incorrect');
 	}
-
-	function ValiderUtilisateur()
-	{
-		if ($_SESSION['username'] == USERNAME1 && $_SESSION['password'] == PASSWORD1) 
-		{
-			header('Location:connexion.php?erreur=9vies est connecté');
-		}
-		else if ($_SESSION['username'] == USERNAME2 && $_SESSION['password'] == PASSWORD2) 
-		{
-			header('Location:connexion.php?erreur=Generalkiwi est connecté');
-		}
-	}
-
  ?>
