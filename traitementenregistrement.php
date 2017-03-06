665f330a5b6c11272138e6ce1e9747ccec176f73
generalkiwi1 <xx-mik-xx@hotmail.com>
2017-03-06 15:50:06 -0500

    Modification de mon mandat

---
+++ traitementenregistrement.php
@@ -0,0 +1,11 @@
+<?php
+	
+	echo $_POST['tournamentname'].'<br>';
+	echo $_POST['maxplayer'].'<br>';
+	echo $_POST['cityname'].'<br>';
+	echo $_POST['countryname'].'<br>';
+	echo $_POST['gamename'].'<br>';
+	echo $_POST['day'].'<br>';
+	echo $_POST['month'].'<br>';
+	echo $_POST['year'].'<br>';
+?>
