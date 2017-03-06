90a187d979d37f14fc28df8a9e15579aec69ea98
9vies <dragonslayer834@gmail.com>
2017-03-06 15:38:09 -0500

    STYLE

--- recherche.php
+++ recherche.php
@@ -6,18 +6,19 @@
 	include "banniere.php";
 ?>	
 	<div id="contenu">
-		<form action="traitementrecherche.php" method="post">
-			Nom d'événement:<input type="text" name="tournamentname" maxlength="30"><br>
-			Année:<select name="year">	
+		<h1 class="title">Rechercher un tournoi</h1><br><br><br>
+		<form action="traitementrecherche.php" method="post" id="rechercheform">
+			Nom d'événement:<input type="text" name="tournamentname" maxlength="30" class="rechercheformelements"><br><br>
+			Année:<select name="year" class="rechercheformelements">	
 					<option value="2015">2015</option>
 					<option value="2016">2016</option>
 					<option value="2017" selected="selected">2017</option>
 					<option value="2018">2018</option>
 					<option value="2019">2019</option>		
-				  </select><br>
-			Ville:<input type="text" name="cityname" maxlength="15"><br>
-			Jeu:<input type="text" name="gamename" maxlength="30"><br>
-			<button type="Submit" value="Submit">Rechercher</button>	
+				  </select><br><br>
+			Ville:<input type="text" name="cityname" maxlength="15" class="rechercheformelements"><br><br>
+			Jeu:<input type="text" name="gamename" maxlength="30" class="rechercheformelements"><br><br>
+			<button type="Submit" value="Submit" class="submitbutton">Rechercher</button>	
 		</form>
 	</div>
 	<div class="cleardiv">
