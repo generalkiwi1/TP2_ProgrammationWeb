e1b43d560a6a20ed632703d99a1477d32242b4ef
9vies <dragonslayer834@gmail.com>
2017-03-06 14:24:49 -0500

    Formulaire

--- recherche.php
+++ recherche.php
@@ -6,7 +6,19 @@
 	include "banniere.php";
 ?>	
 	<div id="contenu">
-		
+		<form action="traitementrecherche.php" method="post">
+			Nom d'événement:<input type="text" name="tournamentname" maxlength="30"><br>
+			Année:<select name="year">	
+					<option value="2015">2015</option>
+					<option value="2016">2016</option>
+					<option value="2017" selected="selected">2017</option>
+					<option value="2018">2018</option>
+					<option value="2019">2019</option>		
+				  </select><br>
+			Ville:<input type="text" name="cityname" maxlength="15"><br>
+			Jeu:<input type="text" name="gamename" maxlength="30"><br>
+			<button type="Submit" value="Submit">Rechercher</button>	
+		</form>
 	</div>
 	<div class="cleardiv">
 		
