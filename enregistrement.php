0b2a11d3e6ccd1de4b1de1a5a6f411c25d24ae96
generalkiwi1 <xx-mik-xx@hotmail.com>
2017-03-06 14:46:09 -0500

    Just a push

--- enregistrement.php
+++ enregistrement.php
@@ -11,7 +11,10 @@
 			Nom d'événement: <input type="text" name="tournamentname" maxlength="30"><br>
 			Nom de la ville: <input type="text" name="cityname" maxlength="15"><br>
 			Nom du jeu: <input type="text" name="gamename" maxlength="30"><br>
-			Date: <input type=""><input type="text"><input type="text">
+			Date: 
+			<select>
+				
+				
 		</form>
 	</div>
 	<div class="cleardiv">
