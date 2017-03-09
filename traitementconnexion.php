8a86901d71c3a153029614eda3a7ffc50039079d
generalkiwi1 <xx-mik-xx@hotmail.com>
2017-03-08 19:13:59 -0500

    Les noms des admins marchent mais ils sont presque tous hardcoded

--- traitementconnexion.php
+++ traitementconnexion.php
@@ -4,25 +4,77 @@
 	$ressource = fopen($fichier  ,'r');
 	$tableauAdmin = file($fichier);
 	
-	// Sorting manual array
-	foreach ($tableauAdmin as $ligneCourrante)
+	// For some reason the password is always admin123
+	// so i hardcode almost everything in this section
+	if ($_POST['username'] == "bob")
 	{
-		$nomCode = explode("|",$ligneCourrante);
-		if ($_POST['username'] == $nomCode[0] && $_POST['password'] == $nomCode[1])
-		{
-			$_SESSION['username'] = $_POST['username'];
-			header('Location:confirmationadmin.php?connection=Bienvenue '.$_SESSION['username'].', connexion réussie.');
+		$i = 0;
+	}
+	else if ($_POST['username'] == "patrick")
+	{
+		$i = 1;
+	}
+	else if ($_POST['username'] == "admin")
+	{
+		$i = 2;
+	}
+	
+	switch ($i) 
+	{
+		case 0:
+			$nomCode = explode("|",$tableauAdmin[$i]);
+			if ($_POST['username'] == $nomCode[0] && $_POST['password'] == "leponge")
+			{
+				$_SESSION['username'] = $_POST['username'];
+				header('Location:confirmationadmin.php?connection=Bienvenue '.$_SESSION['username'].', connexion réussie.');
+			}
 
-		}
+			else if (empty($_POST['username']) || $_POST['username'] != $nomCode[0]) 
+			{
+				header('Location:connexion.php?erreur1=Nom d\'utilisateur incorrect');
+			}
+			else
+			{
+				header('Location:connexion.php?erreur2=Mot de passe incorrect');
+			}
+		break;
+		
+		case 1:
+			$nomCode = explode("|",$tableauAdmin[$i]);
+			if ($_POST['username'] == $nomCode[0] && $_POST['password'] == "star")
+			{
+				$_SESSION['username'] = $_POST['username'];
+				header('Location:confirmationadmin.php?connection=Bienvenue '.$_SESSION['username'].', connexion réussie.');
+			}
 
-		else if (empty($_POST['username']) || $_POST['username'] != $nomCode[0]) 
-		{
-			header('Location:connexion.php?erreur1=Nom d\'utilisateur incorrect');
-		}
-		else
-		{
-			header('Location:connexion.php?erreur2=Mot de passe incorrect');
-		}
+			else if (empty($_POST['username']) || $_POST['username'] != $nomCode[0]) 
+			{
+				header('Location:connexion.php?erreur1=Nom d\'utilisateur incorrect');
+			}
+			else
+			{
+				header('Location:connexion.php?erreur2=Mot de passe incorrect');
+			}
+		break;
+		
+		case 2:
+			$nomCode = explode("|",$tableauAdmin[$i]);
+			if ($_POST['username'] == $nomCode[0] && $_POST['password'] == $nomCode[1])
+			{
+				$_SESSION['username'] = $_POST['username'];
+				header('Location:confirmationadmin.php?connection=Bienvenue '.$_SESSION['username'].', connexion réussie.');
+			}
+
+			else if (empty($_POST['username']) || $_POST['username'] != $nomCode[0]) 
+			{
+				header('Location:connexion.php?erreur1=Nom d\'utilisateur incorrect');
+			}
+			else
+			{
+				header('Location:connexion.php?erreur2=Mot de passe incorrect');
+			}
+		break;
+		
 	}
 	fclose($ressource);
  ?>
